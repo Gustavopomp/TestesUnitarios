b8cae1122fe6ef86bf496dcef9c44fc4085afbd6
GustavoPomp <gustavoferrazpompeo@gmail.com>
2023-10-12 08:29:24 -0300

    inicializando

---
+++ src/main/java/gusystem/api/GuApiApplication.java
@@ -0,0 +1,13 @@
+package gusystem.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gusystem/api/GuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package gusystem.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
